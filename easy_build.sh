1e945038a39d7125a9bff886f85b843e6377a1c4
sko00o <lwylwy@outlook.com>
2025-02-24 14:44:43 +0800

    add easy_build.sh

---
+++ easy_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+CURRENT_DIR="$(dirname "$0")"
+pushd $CURRENT_DIR
+git pull
+popd
+
+TM=$(date +%Y%m%d)
+OUTPUT_DIR="$OUTPUT_DIR:-$HOME/data"
+
+pushd $OUTPUT_DIR
+for i in $@; do
+    NAME=$(basename $i)
+
+    $CURRENT_DIR/build.sh \
+        $CURRENT_DIR/etc/${i}.yaml | tee ${NAME}.log
+
+    mv "${NAME}.qcow2" "${NAME}_${TM}.qcow2"
+done
+popd
